a0de1369b768a6b0e545c9e30b4e6f0cbb9e96a9
hwao-omega <omega-hsu@hwaotech.com>
2020-11-17 00:31:06 +0800

    [fix] eslint 修正

--- src/views/home/components/DrinkDialog.vue
+++ src/views/home/components/DrinkDialog.vue
@@ -1,11 +1,28 @@
 <template>
-  <Dialog ref="dialog" :title="headerTitle">
+  <Dialog
+    ref="dialog"
+    :title="headerTitle"
+  >
     <div slot="dialog-body">
       <v-row>
-        <v-col cols="12" sm="12" md="6">
-          <v-text-field ref="name" v-model="formData.name" :rules="[v => !!v || 'This field is required']" label="飲品" required></v-text-field>
+        <v-col
+          cols="12"
+          sm="12"
+          md="6"
+        >
+          <v-text-field
+            ref="name"
+            v-model="formData.name"
+            :rules="[v => !!v || 'This field is required']"
+            label="飲品"
+            required
+          />
         </v-col>
-        <v-col cols="12" sm="12" md="6">
+        <v-col
+          cols="12"
+          sm="12"
+          md="6"
+        >
           <v-text-field
             ref="price"
             v-model.number="formData.price"
@@ -14,17 +31,50 @@
             label="價錢"
             hint="須為正整數"
             required
-          ></v-text-field>
+          />
         </v-col>
-        <v-col cols="12" sm="12" md="6">
-          <v-textarea v-model="formData.notes" class="note" outlined name="input-7-4" label="multi-line text"></v-textarea>
+        <v-col
+          cols="12"
+          sm="12"
+          md="6"
+        >
+          <v-textarea
+            v-model="formData.notes"
+            class="note"
+            outlined
+            name="input-7-4"
+            label="multi-line text"
+          />
         </v-col>
-        <v-col cols="12" sm="12" md="6">
-          <v-text-field v-model="formData.picture" label="圖片連結" readonly></v-text-field>
+        <v-col
+          cols="12"
+          sm="12"
+          md="6"
+        >
+          <v-text-field
+            v-model="formData.picture"
+            label="圖片連結"
+            readonly
+          />
         </v-col>
         <v-col cols="12">
-          <v-btn class="mr-3" depressed small color="error" @click="close">Close</v-btn>
-          <v-btn depressed small color="primary" @click="submit">Save</v-btn>
+          <v-btn
+            class="mr-3"
+            depressed
+            small
+            color="error"
+            @click="close"
+          >
+            Close
+          </v-btn>
+          <v-btn
+            depressed
+            small
+            color="primary"
+            @click="submit"
+          >
+            Save
+          </v-btn>
         </v-col>
       </v-row>
     </div>
@@ -32,15 +82,13 @@
 </template>
 
 <script>
-import Dialog from '@/components/Dialog';
-import { mapGetters, mapMutations } from 'vuex';
-import _ from 'lodash';
+import Dialog from '@/components/Dialog'
+import { mapGetters, mapMutations } from 'vuex'
+import _ from 'lodash'
 export default {
   inject: ['sampleImg'],
   name: 'DrinkDialog',
-  components: {
-    Dialog,
-  },
+  components: { Dialog },
   data() {
     return {
       type: 'add',
@@ -56,16 +104,16 @@ export default {
         add: this.ADD_ORDERS,
         edit: this.EDIT_ORDERS,
       },
-    };
+    }
   },
   computed: {
     ...mapGetters({ orders: 'orders/orders' }),
     headerTitle() {
-      return 'drink';
+      return 'drink'
     },
   },
   created() {
-    this.formData = _.cloneDeep(this.initFormData);
+    this.formData = _.cloneDeep(this.initFormData)
   },
   methods: {
     ...mapMutations({
@@ -73,40 +121,43 @@ export default {
       EDIT_ORDERS: 'orders/EDIT_ORDERS',
     }),
     show({ action, item, key }) {
-      this.formData = _.cloneDeep(this.initFormData);
-      this.type = action;
+      this.formData = _.cloneDeep(this.initFormData)
+      this.type = action
       switch (action) {
-        case 'add':
-          break;
-        case 'edit':
-          this.key = key;
-          this.formData = { ...item };
-          break;
-        default:
-          break;
+      case 'add':
+        break
+      case 'edit':
+        this.key = key
+        this.formData = { ...item }
+        break
+      default:
+        break
       }
-      this.$refs.dialog.show();
+      this.$refs.dialog.show()
     },
     close() {
-      this.$refs.dialog.hide();
+      this.$refs.dialog.hide()
     },
     submit() {
       let flag = true;
       ['name', 'price'].forEach(item => {
         if (this.$refs[item] && Object.keys(this.$refs[item]).length !== 0) {
           if (!this.$refs[item].validate(true)) {
-            flag = false;
+            flag = false
           }
         }
-      });
+      })
       if (flag) {
-        this.api[this.type]({ data: this.formData, key: this.key });
+        this.api[this.type]({
+          data: this.formData,
+          key: this.key,
+        })
       }
-      this.close();
+      this.close()
       // this.ADD_ORDERS
     },
   },
-};
+}
 </script>
 <style lang="scss">
 .note {
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,43 +1,92 @@
 <template>
   <v-row>
     <v-col cols="12">
-      <v-btn color="blue lighten-2" dark @click="testDialog1 = true"> Click Me </v-btn>
-      <v-dialog v-if="testDialog1" v-model="testDialog1" data-app width="500">
+      <v-btn
+        color="blue lighten-2"
+        dark
+        @click="testDialog1 = true"
+      >
+        Click Me
+      </v-btn>
+      <v-dialog
+        v-if="testDialog1"
+        v-model="testDialog1"
+        data-app
+        width="500"
+      >
         <v-card>
           <v-card-title>ok</v-card-title>
           <v-card-text>omega</v-card-text>
         </v-card>
       </v-dialog>
-      <v-btn icon @click="openDialog({ action: 'add' })">
+      <v-btn
+        icon
+        @click="openDialog({ action: 'add' })"
+      >
         <v-icon>{{ svgPath.mdiPlus }}</v-icon>
       </v-btn>
-      <v-dialog v-model="testDialog" data-app width="500">
+      <v-dialog
+        v-model="testDialog"
+        data-app
+        width="500"
+      >
         <template v-slot:activator="{ on, attrs }">
-          <v-btn color="red lighten-2" dark v-bind="attrs" v-on="on" @click="testDialog = true"> Click Me </v-btn>
+          <v-btn
+            color="red lighten-2"
+            dark
+            v-bind="attrs"
+            v-on="on"
+            @click="testDialog = true"
+          >
+            Click Me
+          </v-btn>
         </template>
         <v-card>
           <v-card-title>ok</v-card-title>
-          <v-card-text>omega</v-card-text>
+          <v-card-text class="omega">
+            omega
+          </v-card-text>
         </v-card>
       </v-dialog>
     </v-col>
-    <v-col v-for="(item, key) in orders" :key="key" cols="12" sm="4" md="3" xl="2">
+    <v-col
+      v-for="(item, key) in orders"
+      :key="key"
+      cols="12"
+      sm="4"
+      md="3"
+      xl="2"
+    >
       <v-card>
-        <v-img :src="sampleImg" gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)" height="200px"> </v-img>
-        <v-card-title class="pb-0"> {{ item.name }} </v-card-title>
+        <v-img
+          :src="sampleImg"
+          gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+          height="200px"
+        />
+        <v-card-title class="pb-0">
+          {{ item.name }}
+        </v-card-title>
         <v-card-text class="text-right">
-          <div class="my-4 subtitle-1">$ {{ item.price }}</div>
+          <div class="my-4 subtitle-1">
+            $ {{ item.price }}
+          </div>
           <div class="text-left text-truncate">
             {{ item.notes }}
           </div>
         </v-card-text>
-        <v-divider></v-divider>
+        <v-divider />
         <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn icon @click="openDialog({ action: 'edit', item, key })">
+          <v-spacer />
+          <v-btn
+            icon
+            @click="openDialog({ action: 'edit', item, key })"
+          >
             <v-icon>{{ svgPath.mdiFileEdit }}</v-icon>
           </v-btn>
-          <v-btn icon @click="deleteDrink({ action: 'delete', item, key })">
+          <v-btn
+            icon
+            @click="deleteDrink({ action: 'delete', item, key })"
+          >
             <v-icon>{{ svgPath.mdiDelete }}</v-icon>
           </v-btn>
         </v-card-actions>
@@ -48,45 +97,51 @@
 </template>
 
 <script>
-import { mapActions, mapGetters, mapMutations } from 'vuex';
-import { mdiDelete, mdiFileEdit, mdiPlus } from '@mdi/js';
-import DrinkDialog from '@/views/home/components/DrinkDialog';
+import { mapActions, mapGetters, mapMutations } from 'vuex'
+import { mdiDelete, mdiFileEdit, mdiPlus } from '@mdi/js'
+import DrinkDialog from '@/views/home/components/DrinkDialog'
 export default {
   inject: ['sampleImg'],
   name: 'Home',
-  components: {
-    DrinkDialog,
-  },
+  components: { DrinkDialog },
   data() {
     return {
       testDialog1: false,
       testDialog: false,
-      svgPath: { mdiDelete, mdiFileEdit, mdiPlus },
-    };
+      svgPath: {
+        mdiDelete,
+        mdiFileEdit,
+        mdiPlus,
+      },
+    }
   },
-  computed: {
-    ...mapGetters({ orders: 'orders/orders' }),
-  },
+  computed: { ...mapGetters({ orders: 'orders/orders' }) },
   created() {
-    this.initOrders();
+    this.initOrders()
     this.orders.map(o => {
-      o.picture = this.sampleImg;
-      return o;
-    });
+      o.picture = this.sampleImg
+      return o
+    })
   },
   methods: {
     ...mapActions({ initOrders: 'orders/initOrders' }),
-    ...mapMutations({ DELETE_ORDER: 'orders/DELETE_ORDER' }),
+    ...mapMutations({ deleteOrder: 'orders/DELETE_ORDER' }),
     deleteDrink({ key }) {
-      this.DELETE_ORDER(key);
+      this.deleteOrder(key)
     },
     openDialog(payload) {
-      this.$refs.DrinkDialog.show(payload);
+      this.$refs.DrinkDialog.show(payload)
     },
   },
-};
+}
 </script>
 <style lang="scss">
+//@import 'src/assets/styles/settings/_color.scss';
+@import '~@/assets/styles/settings/_color.scss';
+.omega {
+  background-color: $message-delete-header-bg;
+}
+//@import "";
 .v-dialog__container {
   //display: unset !important;
 }
