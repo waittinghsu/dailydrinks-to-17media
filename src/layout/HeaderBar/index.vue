c695cde37eb49e9e0c3aef8e42aefdf5f119c988
hwao-omega <omega-hsu@hwaotech.com>
2020-11-17 00:30:36 +0800

    [fix] eslint 修正

--- src/layout/HeaderBar/index.vue
+++ src/layout/HeaderBar/index.vue
@@ -1,7 +1,11 @@
 <template>
-  <v-app-bar dark app :src="headerImg">
+  <v-app-bar
+    dark
+    app
+    :src="headerImg"
+  >
     <v-toolbar-title>{{ title }}</v-toolbar-title>
-    <v-spacer></v-spacer>
+    <v-spacer />
     <slot />
   </v-app-bar>
 </template>
@@ -17,9 +21,7 @@ export default {
     },
   },
   data() {
-    return {
-      headerImg: 'https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg',
-    };
+    return { headerImg: 'https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg' }
   },
-};
+}
 </script>
