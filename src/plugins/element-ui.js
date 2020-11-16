fdfa68bb7570af1bdb3183d602f332851dcf9dc4
hwao-omega <omega-hsu@hwaotech.com>
2020-11-17 00:30:47 +0800

    [fix] eslint 修正

--- src/plugins/element-ui.js
+++ src/plugins/element-ui.js
@@ -1,6 +1,6 @@
-import Vue from 'vue';
-import 'element-ui/lib/theme-chalk/index.css';
-import { Dialog, Loading, Icon } from 'element-ui';
-Vue.use(Dialog);
-Vue.use(Loading);
-Vue.use(Icon);
+import Vue from 'vue'
+import 'element-ui/lib/theme-chalk/index.css'
+import { Dialog, Loading, Icon } from 'element-ui'
+Vue.use(Dialog)
+Vue.use(Loading)
+Vue.use(Icon)
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,13 +1,9 @@
-import '@mdi/font/css/materialdesignicons.css';
-import Vue from 'vue';
-import Vuetify from 'vuetify';
-import 'vuetify/dist/vuetify.min.css';
-Vue.use(Vuetify);
+import '@mdi/font/css/materialdesignicons.css'
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+Vue.use(Vuetify)
 // mdiSvg mdi md
-const opts = {
-  icons: {
-    iconfont: 'mdi',
-  },
-};
+const opts = { icons: { iconfont: 'mdi' } }
 
-export default new Vuetify(opts);
+export default new Vuetify(opts)
